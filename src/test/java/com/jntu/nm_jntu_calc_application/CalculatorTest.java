a7f139632c66fb656f69e9ac7cd255ac3506a522
RVKarthikeyan <karthikeyanrv04@gmail.com>
2025-11-02 14:23:34 +0530

    corrected test script

--- src/test/java/com/jntu/nm_jntu_calc_application/CalculatorTest.java
+++ src/test/java/com/jntu/nm_jntu_calc_application/CalculatorTest.java
@@ -8,7 +8,6 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-     // This is a deliberate failure: expecting 9, but 5+3=8
-        assertEquals("5 + 3 should equal 9", 9, calc.add(5, 3));
+        assertEquals("5 + 3 should equal 8", 8, calc.add(5, 3));
     }
 }
